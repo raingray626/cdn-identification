ac47325fe171844a83f4af50098d34202cc72ea8
gbbsec <54978924+gbbsec@users.noreply.github.com>
2021-04-18 16:37:51 +0800

    订正校验逻辑

--- cdn_identification.sh
+++ cdn_identification.sh
@@ -1,9 +1,14 @@
 for d in $(sort -u ${1})
 do
-    check_status=$(jq -r ".[\"${d}\"].name" ./cdn.json)
+    check_domain=$(host -t cname $d | grep 'is an alias for' | awk '{print $6}' | gawk 'BEGIN{FS="."; OFS="."} {print $(NF-2),$(NF-1)}')
+    if [[ -z $check_domain ]]; then
+        continue
+    fi
+
+    check_status=$(jq -r ".[\"${check_domain}\"].name" ./cdn.json)
     if [[ $check_status != 'null' ]]; then
         echo $d,$check_status
     else
-        echo "$d,$check_status"
+        echo "$d,$check_domain,$check_status"
     fi
 done
